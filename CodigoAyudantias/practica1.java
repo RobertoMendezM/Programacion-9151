5cf8201fe8f9c21b434bf59bfabe6396340ac9c3
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-03-15 09:49:48 -0600

    clase 15-03

---
+++ CodigoAyudantias/practica1.java
@@ -0,0 +1,11 @@
+public class practica1 {
+    public static void main(String[] args) {
+        // #### Ejercicio 1 ###
+        // leer un numero que tenga la opcion de la formula
+        if (numero == 2) {
+        
+            // solicitas 2 numeros más y haces la operacion
+            // imprimes el resultado. 
+        }
+    }
+}
