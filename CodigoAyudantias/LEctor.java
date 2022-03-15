c2f997a0bda6cd70d246791f01351e437385c1ee
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-03-15 08:19:04 -0600

    clases antes del 15-03

---
+++ CodigoAyudantias/LEctor.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.io.File;
+
+public class LEctor {
+
+    public static void main(String[] args) {
+
+        try {
+            Scanner escaner = new Scanner(System.in); // Creando un obj scanner
+            File miArchivo = new File("AMZN.csv"); // Creando un obj archivo o File
+
+            escaner = new Scanner(miArchivo);
+
+            while (escaner.hasNextLine()) {
+                String linea = escaner.nextLine(); // Leemos la siguiente linea del archivo.
+                System.out.println(linea);
+            } 
+
+        } catch (Exception miError) {
+            System.out.println("Hola no encontre el archivo. :( ");
+            miError.printStackTrace();
+
+        }
+
+
+
+
+        
+
+    }
+   
+
+
+}
--- CodigoAyudantias/tipos.java
+++ CodigoAyudantias/tipos.java
@@ -1,19 +1,47 @@
 public class tipos {
     public static void main(String[] args) {
         //Scanner
-        int entero = 10; 
+        int entero = 3; 
         String cadenaDeTexto = "Hola " ;
         float flotante = 50.25f;
         double doble = 20;
+        
 
+        // IF... ELSE
+        if (entero > 5) {
+            System.out.println("Entero es mayor que 5");
+        }
+        else {
+            System.out.println("Entero NO es mayor que 5");
+        }
+        boolean bool = false;
+        boolean bool2 = true;
+        // Algunos operadores logicos
+        // ! -> NEgacion
+        // && -> and
+        // || -> or
+        if ( (bool2 && bool) || bool2  ) {
+            System.out.println("Entro el if");
+        } else {
+            System.out.println("Entro el else");
+        }
+
+        System.out.println("###");
         // i++ <--> i = i + 1
-        for (int i = 0; i <= n ; i++) {
+        for (int i = 0; i <= 10 ; i++) {
             System.out.println(i);
         }
 
-        for (int i = 0; i <= 10; i= i +1 ) {
-            System.out.println(i);
-        }
+        //System.out.println("###");
+        //for (int i = 0; i <= 10; i= i +1 ) {
+        //    System.out.println(i);
+        //}
+
+        
+
+
+
+
 
 
     }
