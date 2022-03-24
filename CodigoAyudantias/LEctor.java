5953b9c0cae9ee3b758e2fbcde7aafeb726bdc20
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-03-24 09:00:29 -0600

    clase 24-03

--- CodigoAyudantias/LEctor.java
+++ CodigoAyudantias/LEctor.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.io.File;
+import java.util.Arrays;
 
 public class LEctor {
 
@@ -7,7 +8,7 @@ public class LEctor {
 
         try {
             Scanner escaner = new Scanner(System.in); // Creando un obj scanner
-            File miArchivo = new File("AMZ0oN.csv"); // Creando un obj archivo o File
+            File miArchivo = new File("AMZN.csv"); // Creando un obj archivo o File
 
             escaner = new Scanner(miArchivo);
 
@@ -16,6 +17,25 @@ public class LEctor {
                 //int numero = escaner.nextInt(); // Leemos el siguiente entero del archivo.
                 System.out.println(linea);
             } 
+
+            escaner = new Scanner(miArchivo);
+
+            String[] valores = new String[7];
+            int contadorLinea= 0;
+            while (escaner.hasNextLine()) {
+                contadorLinea++;
+                String linea = escaner.nextLine(); // Leemos la siguiente linea del archivo.
+                if (contadorLinea > 1) {
+                    valores = linea.split(",");
+                    //System.out.println(Arrays.toString(valores));
+                    System.out.println(Double.parseDouble(valores[5]) + 20);
+                }
+            }
+
+            
+
+
+
 
         } catch (Exception miError) {
             System.out.println("Hola no encontre el archivo. :( ");
---
+++ CodigoAyudantias/dudas.java
@@ -0,0 +1,31 @@
+color = "naranja"
+if(color == "negro"){
+    // algun codigoo
+}
+else if(color empieza con "n") { 
+    // algun codigooo
+} 
+else if() {
+
+}
+else {
+    // Si no entre a ningun anterior se corre este. 
+}
+
+
+    A
+---------
+  B + C
+
+while(B + c == 0){
+    System.out.println("Division entre cero!")
+    // dame otro valor de B y de C
+    b= scanner.nextInt();
+    c = scanner.nextInt();
+}
+
+color = "rojo"
+while(color == "rojo"){
+    System.out.println("El color no deberia ser rojo!!!")
+    color = scanner.nextLine();
+}
