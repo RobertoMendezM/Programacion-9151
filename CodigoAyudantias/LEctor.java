34d4b608e5e2fd6046367766404e85634dabc700
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-03-29 10:08:14 -0600

    clase 29-03

--- CodigoAyudantias/LEctor.java
+++ CodigoAyudantias/LEctor.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.File;
 import java.util.Arrays;
+import java.io.FileWriter;
 
 public class LEctor {
 
@@ -11,6 +12,8 @@ public class LEctor {
             File miArchivo = new File("AMZN.csv"); // Creando un obj archivo o File
 
             escaner = new Scanner(miArchivo);
+
+            FileWriter escritor = new FileWriter("AMZN_copia.csv"); // Creando un obj escritor
 
             while (escaner.hasNextLine()) {
                 String linea = escaner.nextLine(); // Leemos la siguiente linea del archivo.
@@ -25,14 +28,20 @@ public class LEctor {
             while (escaner.hasNextLine()) {
                 contadorLinea++;
                 String linea = escaner.nextLine(); // Leemos la siguiente linea del archivo.
+                if(contadorLinea == 1){
+                    escritor.write(linea + "\n");
+                }
                 if (contadorLinea > 1) {
                     valores = linea.split(",");
                     //System.out.println(Arrays.toString(valores));
                     System.out.println(Double.parseDouble(valores[5]) + 20);
+                    escritor.write(valores[0]+ ","+   valores[1] + ","+ valores[2] + "," +  valores[3] 
+                                    +","+ valores[4] +"," + (Double.parseDouble(valores[5]) + 20) + "\n");
                 }
             }
 
-            
+            escaner.close();
+            escritor.close();
 
 
 
