e13e37319c0d8b15939085e0e59eccb7b2f5c1aa
RobertoMendezM <91458910+RobertoMendezM@users.noreply.github.com>
2022-04-07 08:02:42 -0500

    Add files via upload

--- Representación Numérica- PuntoFlotante/EpsilonDeMaquina.py
+++ Representación Numérica- PuntoFlotante/EpsilonDeMaquina.py
@@ -3,21 +3,25 @@
 Programa que calcula el epsilon de máquina
 
 Created on Wed Mar 23 05:50:20 2022
+update: Thu Apr 7
 
 @author: Roberto Méndez Méndez
 """
 
 class EpsilonDeMaquina:
     
+    # Calculo correcto
     def epsilonMaquina(self):
         eps, i = 1.0 , 0
         while 1 + eps > 1:
             eps /= 2
             i += 1
-        print("El epsilon de máquina es: ", eps)
+        epsMachine = eps*2    
+        print("El epsilon de máquina es: ", epsMachine)
         print("La potencia es: ", i)    
         
-        
+    
+    # Calculo incorrecto    
     def epsilonMaquinaSad(self):
         eps, i = 1.0 , 0
         while eps > 0:
