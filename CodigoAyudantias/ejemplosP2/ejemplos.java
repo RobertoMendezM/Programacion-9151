f1564998aee79d53f900af693e68375138b7e1aa
Ulmo Macias <jorgemacias@ciencias.unam.mx>
2022-04-28 09:03:17 -0500

    clase 28/04

--- CodigoAyudantias/ejemplosP2/ejemplos.java
+++ CodigoAyudantias/ejemplosP2/ejemplos.java
@@ -5,9 +5,10 @@ import java.util.Arrays;
 public class ejemplos {
     public static void main(String[] args) {
         try {
+            int mesNumerico = 0;
             Scanner escaner = new Scanner(System.in); // Creando un obj scanner
             File miArchivo = new File("GOOGL.csv"); // Creando un obj archivo o File
-
+            
             escaner = new Scanner(miArchivo);
             int contadorLinea = 0;
             String[] valores = new String[7];
@@ -15,22 +16,32 @@ public class ejemplos {
             while (escaner.hasNextLine()) {
                 String linea = escaner.nextLine(); // Leemos la siguiente linea del archivo.
                 contadorLinea++;
-
-
                 if (contadorLinea > 1) {
                     // int numero = escaner.nextInt(); // Leemos el siguiente entero del archivo.
                     //System.out.println(linea);
                     valores = linea.split(",");
                     valoresColumna[contadorLinea-2] = Integer.parseInt(valores[6]);
+                    //System.out.println(Arrays.toString(valores[0].split("-")));
+                    String mes = valores[0].split("-")[1];
+                    mesNumerico = mesNumerico + Integer.parseInt(mes);
+                    if(mes.equals("01") || mes.equals("2")){
+                        System.out.println("enero");
+                    }
+                    if (mes.equals("12")) {
+                        System.out.println("Diciembre");
+                    }
+                    
                     //System.out.println(valores[6]);
                     
                 }
 
             }
-            System.out.println(Arrays.toString(valoresColumna));
+            System.out.println(mesNumerico);
+            //System.out.println(Arrays.toString(valoresColumna));
             Arrays.sort(valoresColumna);
             System.out.println("#########################");
-            System.out.println(Arrays.toString(valoresColumna));
+            //System.out.println(Arrays.toString(valoresColumna));
+            escaner.close();
 
 
 
@@ -40,7 +51,7 @@ public class ejemplos {
     {
         System.out.println("Hola no encontre el archivo. :( ");
         miError.printStackTrace();
-
+        
     }
     }
 }
